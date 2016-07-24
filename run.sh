c92953ebc34e24d807ab43ad0275f72c9aea868e
Adam Barth <abarth@chromium.org>
2016-07-23 18:55:26 -0700

    Position cursor

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 set -e
 
 mkdir -p out
-gcc --std=c++11 -Werror -Wall -Wextra zi.cc -o out/zi
+g++ --std=c++11 -Werror -Wall -Wextra zi.cc -o out/zi
 ./out/zi
--- zi.cc
+++ zi.cc
@@ -14,44 +14,130 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
+
+#include <sstream>
+#include <string>
+#include <utility>
 
 #define ESC "\x1B"
 
 namespace term {
+namespace {
+
+int cols = 0;
+int rows = 0;
+
+}  // namespace
+
+constexpr char kSaveScreen[] = ESC "[?47h";
+constexpr char kRestoreScreen[] = ESC "[?47l";
+constexpr char kEraseScreen[] = ESC "[2J";
+constexpr char kEraseToEndOfLine[] = ESC "[K";
+// constexpr char kClearCharacterAttributes[] = ESC "[0m";
+// constexpr char kSetBold[] = ESC "[1m";
+// constexpr char kSetLowIntensity[] = ESC "[2m";
+// constexpr char kSetUnderline[] = ESC "[4m";
+// constexpr char kSetReverseVideo[] = ESC "[7m";
+// constexpr char kSetInvisibleText[] = ESC "[8m";
 
 template <size_t n>
 void Put(const char (&message)[n]) {
   write(STDOUT_FILENO, message, n);
 }
 
-void SaveScreen() {
-  Put(ESC "[?47h");
+void Put(const std::string& message) {
+  write(STDOUT_FILENO, message.data(), message.size());
 }
 
-void RestoreScreen() {
-  Put(ESC "[?47l");
+bool GetSize() {
+  struct winsize screen_size;
+  int result = ioctl(STDIN_FILENO, TIOCGWINSZ, &screen_size);
+  if (result == -1)
+    return false;
+  cols = screen_size.ws_col;
+  rows = screen_size.ws_row;
+  return true;
 }
 
-void ClearScreen() {
-  Put(ESC "[2J");
-}
+enum class Color { Black, Red, Green, Yellow, Blue, Magenta, Cyan, White };
+
+const char* kForegroundColors[] = {
+    "30", "31", "32", "33", "34", "35", "36", "37",
+};
+
+const char* kBackgroundColors[] = {
+    "40", "41", "42", "43", "44", "45", "46", "47",
+};
 
 }  // namespace
 
-void Run() {
+namespace zi {
+
+class CommandBuffer {
+ public:
+  template <typename T>
+  CommandBuffer& operator<<(T&& value) {
+    stream_ << value;
+    return *this;
+  }
+
+  void MoveCursorTo(int x, int y);
+  void Execute();
+
+ private:
+  std::ostringstream stream_;
+};
+
+void CommandBuffer::MoveCursorTo(int x, int y) {
+  stream_ << ESC "[" << y << ";" << x << "H";
+}
+
+void CommandBuffer::Execute() {
+  term::Put(stream_.str());
+}
+
+class Shell {
+ public:
+  void set_status(std::string status) { status_ = std::move(status); }
+
+  void Display();
+  void Run();
+
+ private:
+  std::string status_;
+};
+
+void Shell::Display() {
+  CommandBuffer commands;
+  commands << term::kEraseScreen;
+  commands.MoveCursorTo(0, term::rows - 1);
+  commands << status_ << term::kEraseToEndOfLine;
+  commands.Execute();
+}
+
+void Shell::Run() {
   for (;;) {
+    Display();
+
     int c = getchar();
     if (c == 'q')
       return;
   }
 }
 
+}  // zi
+
 int main(int, char**) {
-  term::SaveScreen();
-  term::ClearScreen();
-  term::Put("hello, world\n");
-  Run();
-  term::RestoreScreen();
+  if (!term::GetSize()) {
+    fprintf(stderr, "error: Unable to get terminal size.\n");
+    return 1;
+  }
+  term::Put(term::kSaveScreen);
+  zi::Shell shell;
+  shell.set_status("Hello, world.");
+  shell.Run();
+  term::Put(term::kRestoreScreen);
   return 0;
 }
